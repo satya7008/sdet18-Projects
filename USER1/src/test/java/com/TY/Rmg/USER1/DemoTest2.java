371d6bb3d4c2d3a1b2bf778e02abb4d970b5a883
satyaswain7008@gmail.com <satyaswain7008@gmail.com>
2021-08-04 10:55:06 +0530

    modified class

--- USER1/src/test/java/com/TY/Rmg/USER1/DemoTest2.java
+++ USER1/src/test/java/com/TY/Rmg/USER1/DemoTest2.java
@@ -3,6 +3,7 @@ package com.TY.Rmg.USER1;
 public class DemoTest2 {
 	public static void main(String[] args) {
 		System.out.println("This is my 2nd commit");
+		System.out.println("this is my 2nd class and modified class");
 	}
 
 }
